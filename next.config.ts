9dcf21517e8f29b09b2eb0fe0e9e55b13bfb9727
tam-tamshow <tam.tamshow9@gmail.com>
2025-12-29 14:51:21 +0900

    調整

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,12 @@ import type { NextConfig } from "next";
 
 const repo = "note-quiz"; // ← GitHubのリポジトリ名
 const isProd = process.env.NODE_ENV === "production";
+const basePath = isProd ? `/${repo}` : "";
 
 const nextConfig: NextConfig = {
   output: "export",
 
   // GitHub Pages 用設定（本番のみ有効）
-  basePath: isProd ? `/${repo}` : "",
   assetPrefix: isProd ? `/${repo}/` : "",
 
   // Pagesでは画像最適化サーバーが使えない
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
-          src=`${basePath}/next.svg`
+          src={`${basePath}/next.svg`}
           alt="Next.js logo"
           width={100}
           height={20}
@@ -16,7 +16,7 @@ export default function Home() {
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+            To get started, edit the page.tsx file.a
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
             Looking for a starting point or more instructions? Head over to{" "}
